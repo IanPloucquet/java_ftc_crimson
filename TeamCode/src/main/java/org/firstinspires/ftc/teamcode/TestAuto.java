30b206a2243c0a7fa729d89ce8e5fd0d0a58bb41
IanPloucquet <iploucquet@gmail.com>
2022-11-09 16:33:42 -0500

    Initial Commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -11,9 +11,7 @@ import java.lang.*;
 
 import static java.lang.Math.PI;
 
-import java.lang.Math.*;
 
-import java.lang.Math;
 @Autonomous(name = "Auto")
 public class TestAuto extends LinearOpMode {
 
@@ -28,8 +26,7 @@ public class TestAuto extends LinearOpMode {
             (WHEEL_DIAMETER_INCHES * PI);
 
     public int inchestoticks(int inches) {
-        double tickperInch = 360 / (3.77 * PI);
-        return (int) (inches * tickperInch);//diameter of the wheel 3.77
+        return (int) (inches * COUNTS_PER_INCH);//diameter of the wheel 3.77
     }
 
     @Override
@@ -47,8 +44,8 @@ public class TestAuto extends LinearOpMode {
             backRightEx.setDirection(DcMotorEx.Direction.REVERSE);
             frontRightEx.setDirection(DcMotorEx.Direction.REVERSE);
 
-            forward(20, .9);
-            sleep(2000); // sleep stalls what is written before it for a set amount of time, it won't work if added to a method as a field
+            forward(20, .8);
+            //sleep doesn't work
         }
     }
 
@@ -75,8 +72,18 @@ public class TestAuto extends LinearOpMode {
             frontRightEx.setVelocity(power);
             backLeftEx.setVelocity(power);
             backRightEx.setVelocity(power);
-
         }
+
+        frontLeftEx.getCurrentPosition();
+        frontRightEx.getCurrentPosition();
+        backLeftEx.getCurrentPosition();
+        backLeftEx.getCurrentPosition();
+
+            /*frontLeftEx.setVelocity(0);
+            frontRightEx.setVelocity(0);
+            backLeftEx.setVelocity(0);
+            backRightEx.setVelocity(0);*/
+
     }
 
     public void backward(int inches, double power) {
@@ -105,7 +112,7 @@ public class TestAuto extends LinearOpMode {
 
         }
     }
-        public void right (int inches, double power) {
+        public void right(int inches, double power) {
             frontRightEx.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
             frontLeftEx.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
             backLeftEx.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
@@ -128,7 +135,58 @@ public class TestAuto extends LinearOpMode {
                 frontRightEx.setPower(-power);
                 backLeftEx.setPower(-power);
                 backRightEx.setPower(power);
-
             }
     }
+        public void left(int inches, double power) {
+            frontRightEx.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+            frontLeftEx.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+            backLeftEx.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+            backRightEx.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+
+            frontLeftEx.setTargetPosition((int) (inchestoticks(inches)));
+            frontRightEx.setTargetPosition((int) (inchestoticks(inches)));
+            backRightEx.setTargetPosition((int) (inchestoticks(inches)));
+            backLeftEx.setTargetPosition((int) (inchestoticks(inches)));
+
+
+            frontRightEx.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+            frontLeftEx.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+            backLeftEx.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+            backRightEx.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+
+            if (inches >= 0) {
+                frontLeftEx.setPower(-power);
+                frontRightEx.setPower(power);
+                backLeftEx.setPower(power);
+                backRightEx.setPower(-power);
+            }
+
+    }
+
+  public void turnleft(int inches, double power){
+      frontRightEx.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+      frontLeftEx.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+      backLeftEx.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+      backRightEx.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+
+      frontLeftEx.setTargetPosition((int) (inchestoticks(inches)));
+      frontRightEx.setTargetPosition((int) (inchestoticks(inches)));
+      backRightEx.setTargetPosition((int) (inchestoticks(inches)));
+      backLeftEx.setTargetPosition((int) (inchestoticks(inches)));
+
+
+      frontRightEx.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+      frontLeftEx.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+      backLeftEx.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+      backRightEx.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+
+      if (inches >= 0) {
+          frontLeftEx.setPower(-power);
+          frontRightEx.setPower(power);
+          backLeftEx.setPower(power);
+          backRightEx.setPower(-power);
+      }
+  }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
@@ -5,6 +5,8 @@ import android.net.http.SslCertificate;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 @TeleOp
 public class TestTeleop extends OpMode {
     // frontLeft, frontRight, backLeft, backRight are the names of the motors that will be used
@@ -13,7 +15,7 @@ public class TestTeleop extends OpMode {
     DcMotor frontRight;
     DcMotor backLeft;
     DcMotor backRight;
-
+    double speed = 0.6;
     @Override
     public void init() {
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
@@ -28,33 +30,25 @@ public class TestTeleop extends OpMode {
 
     @Override
     public void loop() {
-       double x = gamepad1.right_stick_x;
-       double y = -gamepad1.left_stick_y;
-       double r = -gamepad1.left_stick_x;
 
-       frontLeft.setPower(y-x+r);
-       frontRight.setPower(y+x-r);
-       backLeft.setPower(y-x-r);
-       backRight.setPower(y+x+r);
+        double x = gamepad1.right_stick_x * speed;
+        double y = gamepad1.left_stick_y * speed;
+        double r = gamepad1.left_stick_x * speed;
 
-
-       // allows for input to gamepad to translate to movement;
-        // Movement of stick sets power for motors
-        if (gamepad1.right_bumper) {
-            double c = gamepad1.right_stick_x;
-            double d = -gamepad1.left_stick_y;
-            double t = -gamepad1.left_stick_x;
-            frontLeft.setPower(0.2);
-            frontRight.setPower(0.2);
-            backLeft.setPower(0.2);
-            backRight.setPower(0.2);
-
-
-            frontLeft.setPower(d-c+t);
-            frontRight.setPower(d+c-t);
-            backLeft.setPower(d-c-t);
-            backRight.setPower(d+c+t);
+        frontLeft.setPower(-y-x-r);
+        frontRight.setPower(-y+x+r);
+        backLeft.setPower(-y-x+r);
+        backRight.setPower(-y+x-r);
+        if (gamepad1.right_bumper && speed < 0.8) {
+            speed += 0.01;
         }
+        if (gamepad1.left_bumper && speed > 0.3) {
+            speed -= 0.01;
+        }
+
+        // allows for input to gamepad to translate to movement;
+        // Movement of stick sets power for motors
+
     }
 }
 
